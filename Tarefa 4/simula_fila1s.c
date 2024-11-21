f7f888e769e9f034cc2190cb157aefad42ee1ac0
username <pedropessoamonteiro@gmail.com>
2024-11-21 18:21:28 +0000

    ai faltava isto

--- Tarefa 4/simula_fila1s.c
+++ Tarefa 4/simula_fila1s.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Invoke the report generator and end the simulation. */
-	report(&state, &stats, &files, &events);
+	report(&state, &stats, &files, &events, &q1);
 	fclose(files.infile);
 	fclose(files.outfile);
 	
